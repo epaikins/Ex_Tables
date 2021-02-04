55f099cc170663560128d6c85e0db3fe34844951
Eugene Pokoo-Aikins <eugene.pokoo-aikins@amalitech.org>
2021-02-04 11:59:15 +0000

    Added two methods readIntoArray and printArray

--- src/Tables.java
+++ src/Tables.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Tables {
+    private static Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+    public static void main(String[] args) {
+        int userInput = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] array = readIntoArray(userInput);
+        printArray(array);
+
+    }
+
+    public static int[] readIntoArray(int sizeOfArray){
+        int[] userArray = new int[sizeOfArray];
+        for(int i = 0; i < sizeOfArray; i++){
+            int mark = scanner.nextInt();
+            userArray[i] = mark;
+        }
+        return userArray;
+    }
+
+    public static void printArray(int[] array){
+        System.out.println(Arrays.toString(array));
+    }
 }
